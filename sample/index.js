83f87586582a442070903c94c6f9582a79e97a06
punchh_dishant <dishant@punchh.com>
2018-07-06 16:30:32 +0530

    Firebase Analytics work for Android

--- sample/index.js
+++ sample/index.js
@@ -9,20 +9,30 @@ import { AppRegistry, Text, TouchableOpacity, View } from 'react-native';
 import RNQuickFirebase from 'react-native-quick-firebase';
 
 class App extends Component {
+
+	componentDidMount() {
+		RNQuickFirebase.setAnalyticsEnabled(true);
+		RNQuickFirebase.setScreenName("Check OTP Screen");
+	  }
+
 	onSendOTP = async () => {
 		console.warn('click');
-
+		let params =null;
 		try {
 			const phoneNumber = '+919799990699';
 			await RNQuickFirebase.sendOTP(phoneNumber);
 			console.warn('otp sent to: ' + phoneNumber);
 			const sessionToken = await RNQuickFirebase.validateOTP('123456');
 			console.warn('OTP validated successfully with sessionToken: ' + sessionToken);
+			params = {["OTP_validate_Event"]: "success"}
+			RNQuickFirebase.logEvent("OTP Send",params);
 			/// send this token to server to cross validate
 			RNQuickFirebase.signOut();
 			console.warn('User Logout from the Firebase');
 		} catch (e) {
 			console.warn(e);
+			params = {["OTP_send_Event"]: "failed"}
+			RNQuickFirebase.logEvent("OTP Send",params);
 		}
 	};
 
